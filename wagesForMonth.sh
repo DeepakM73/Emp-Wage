ee50dc1baff3d2d8b75b8d158275b5fa7bf65ec8
DeepakM73 <deepak.muddasani73@gmail.com>
2021-07-28 00:43:03 +0530

    Added : Month wage

---
+++ wagesForMonth.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+function cal_wage_for_month()
+{
+  count=0
+  hour=0
+  calculatinghour=0
+  total_wage=1
+  while [[ $count -le 20 ]] && [[ $hour -le 100 ]]
+  do
+       hour=$( gethour $(()) )
+       calculatinghour=$(( $calculatinghour + hour ))
+       res=$( check_Employee_wage $(()) )
+       total_wage=$(( $total_wage + res ))
+       storeing_Wages[$count]=$res
+       ((count++))
+  if [ $count -eq 20 ]
+  then
+       break;
+  fi
+  done
+       echo "total wages are: $total_wage"
+       echo "total days are:  ${!storeing_Wages[@]}"
+       echo "daily wages are: ${storeing_Wages[@]}"
+ }
+cal_wage_for_month
